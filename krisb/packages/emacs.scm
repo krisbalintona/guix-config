4ce675ba4fe581653b9bace782e8a8ae4af6e221
Kristoffer Balintona <krisbalintona@gmail.com>
2025-05-24 14:29:40 -0500

    Use custom commit for emacs-master-custom

--- krisb/packages/emacs.scm
+++ krisb/packages/emacs.scm
@@ -1,13 +1,22 @@
 (define-module (krisb packages emacs)
   #:use-module (guix packages)
-  #:use-module ((emacs-master) :prefix emacs-master:)
-  ;; #:use-module (gnu packages emacs)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (guix git-download)
+  #:use-module ((emacs-master) :prefix emacs-master:))
 
 (define-public emacs-master-custom
   (package
     (inherit emacs-master:emacs-master)
     (name "emacs-master-custom")
+    (source
+     ;; I inherit from the original package’s origin but replace the
+     ;; commit (and its associated hash)
+     (origin
+       (inherit (package-source emacs-master:emacs-master))
+       (uri (git-reference
+             (url "https://github.com/emacs-mirror/emacs.git")
+             (commit "492adf20b91520e96fb198e6e936e3145359c43b")))
+       (sha256 (base32 "06varjkp65k7m0kkf3gq2ykig3hwxmainncl0lx6ihz923ymnspl"))))
     (arguments
      (substitute-keyword-arguments (package-arguments emacs-master:emacs-master)
        ((#:configure-flags flags)
