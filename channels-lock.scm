1c7b879ce87a83056efc83041b7f6c1314ef5604
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-09 18:32:23 -0500

    Upgrade packages

--- channels-lock.scm
+++ channels-lock.scm
@@ -3,7 +3,7 @@
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "68bb075f860d5eb138bbcadaadd57381a7822c29")
+          "d80b43c9fe065a2f1e51b45e8f12c5763f683539")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -14,7 +14,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "02270b585e7d641afabd86ee6eaf7a6aad2f5df7")
+          "fb6696c598f630ff46fe542a0523d47a788abaa0")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -22,10 +22,10 @@
               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
         (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
+        (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "ad8cb7af8fc572bb3d11cd0344bd2bed3b9d653f")
+          "03ce76718c41b32d174713c945d269d6fcdd8bf1")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
