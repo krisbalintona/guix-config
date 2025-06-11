bbbadfdb4bc6def89d26c4e48824fcffbb22de4f
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-11 16:01:17 -0500

    fix(keychain.fish): Setting SHELL env in fish

--- conf/files/fish/keychain.fish
+++ conf/files/fish/keychain.fish
@@ -1,8 +1,8 @@
-# This line might be necessary depending on the value of SHELL.  See
-# https://github.com/fish-shell/fish-shell/issues/4583#issuecomment-1408657051
-set -lx SHELL fish
-
 if status is-login
     and status is-interactive
+    # Set SHELL, which keychain uses to infer how to source a keychain
+    # command.  Taken from
+    # https://github.com/fish-shell/fish-shell/issues/4583#issuecomment-1408657051
+    set -lx SHELL fish
     keychain --quick --quiet --ssh-allow-forwarded --eval id_ed25519 --noask | source
 end
