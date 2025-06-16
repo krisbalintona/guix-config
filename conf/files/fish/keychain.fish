f33e9dca6f1d87c7661e70f4344bf13cd127daa3
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-16 02:07:37 -0500

    Clarify comment

--- conf/files/fish/keychain.fish
+++ conf/files/fish/keychain.fish
@@ -1,7 +1,9 @@
 if status is-login
     and status is-interactive
-    # Set SHELL, which keychain uses to infer how to source a keychain
-    # command.  Taken from
+    # Ensure SHELL is properly.  Keychain uses this to infer how to
+    # source a keychain command.  It may be incorrectly set in some
+    # cases, e.g., calling fish manually from within another shell.
+    # Taken from
     # https://github.com/fish-shell/fish-shell/issues/4583#issuecomment-1408657051
     set -lx SHELL fish
     keychain --quick --quiet --ssh-allow-forwarded --eval id_ed25519 --noask | source
