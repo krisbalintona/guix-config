17a01f877f001e61476ae6c0bc0e1b3ace815bbe
Kristoffer Balintona <krisbalintona@gmail.com>
2025-11-17 13:49:54 -0600

    keychain: Fix bash and fish command options
    Seems like --ssh-allow-forwarded was breaking things for some reason?
    
    Also add explanatory comments about what this keychain command does.

--- conf/files/bash/keychain.bash
+++ conf/files/bash/keychain.bash
@@ -1,1 +1,7 @@
-eval `keychain --quick --quiet --ssh-allow-forwarded --eval id_ed25519 --noask`
+# Call keychain, but:
+# 1. Use a running ssh-agent process if there is one.
+# 2. Don't print any messages.
+# 3. Add my personal SSH key but don't prompt for a password.
+#    (Means SSH key will be cached only if its password has been
+#    prompted for another way, e.g., a git push.)
+eval `keychain --quick --quiet --eval id_ed25519 --noask`
--- conf/files/fish/keychain.fish
+++ conf/files/fish/keychain.fish
@@ -6,5 +6,11 @@ if status is-login
     # Taken from
     # https://github.com/fish-shell/fish-shell/issues/4583#issuecomment-1408657051
     set -lx SHELL fish
-    keychain --quick --quiet --ssh-allow-forwarded --eval id_ed25519 --noask | source
+    # Call keychain, but:
+    # 1. Use a running ssh-agent process if there is one.
+    # 2. Don't print any messages.
+    # 3. Add my personal SSH key but don't prompt for a password.
+    #    (Means SSH key will be cached only if its password has been
+    #    prompted for another way, e.g., a git push.)
+    keychain --quick --quiet --eval id_ed25519 --noask | source
 end
