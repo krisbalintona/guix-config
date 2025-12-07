03a79f835bfe986dd6b220685f78b5425ddcc553
Kristoffer Balintona <krisbalintona@gmail.com>
2025-12-06 19:34:29 -0600

    style: Fix whitespace

--- conf/sublation-home.scm
+++ conf/sublation-home.scm
@@ -15,63 +15,63 @@
 (define home-config
   (home-environment
     (services
-      (append
-        (list
-         (simple-service 'copyparty-server
-                         home-shepherd-service-type
-                         (list
-                          (shepherd-service
-                           (provision '(copyparty-server))
-                           (documentation "Start my Copyparty server.")
-                           (auto-start? #t)
-                           (start #~(make-forkexec-constructor
-                                     '(#$(file-append python "/bin/python3")
-                                       #$(local-file "files/copyparty/copyparty-sfx.py")
-                                       "-c" #$(local-file "files/copyparty/copyparty.conf"))))
-                           (stop #~(make-kill-destructor))
-                           (respawn? #t)
-                           (respawn-delay 2))))
-         (service home-bash-service-type
-                  (home-bash-configuration
-                   (bashrc
-                    (list
-                     ;; 2025-12-06: My default shell is bash because I
-                     ;; haven't gotten Tramp to work with Fish shell
-                     ;; yet.  (I've deduced that the prompt is not the
-                     ;; problem.)  So, instead, I keep bash as my
-                     ;; shell and dispatch to fish if the current
-                     ;; process wasn't started by tramp
-                     (plain-file "to-fish.bash"
-                                 "if [[ $- == *i* ]] && [[ ! \"$TERM\" =~ dumb ]]; then
+     (append
+      (list
+       (simple-service 'copyparty-server
+           home-shepherd-service-type
+         (list
+          (shepherd-service
+            (provision '(copyparty-server))
+            (documentation "Start my Copyparty server.")
+            (auto-start? #t)
+            (start #~(make-forkexec-constructor
+                      '(#$(file-append python "/bin/python3")
+                        #$(local-file "files/copyparty/copyparty-sfx.py")
+                        "-c" #$(local-file "files/copyparty/copyparty.conf"))))
+            (stop #~(make-kill-destructor))
+            (respawn? #t)
+            (respawn-delay 2))))
+       (service home-bash-service-type
+         (home-bash-configuration
+           (bashrc
+            (list
+             ;; 2025-12-06: My default shell is bash because I
+             ;; haven't gotten Tramp to work with Fish shell
+             ;; yet.  (I've deduced that the prompt is not the
+             ;; problem.)  So, instead, I keep bash as my
+             ;; shell and dispatch to fish if the current
+             ;; process wasn't started by tramp
+             (plain-file "to-fish.bash"
+               "if [[ $- == *i* ]] && [[ ! \"$TERM\" =~ dumb ]]; then
     exec fish
 fi")))))
-         (service home-fish-service-type)
-         (simple-service 'krisb-symlink-git-config-files-service-type
-                         home-xdg-configuration-files-service-type
-                         `(("git/config"
-                            ,(local-file "files/git/config"))))
-         (simple-service 'krisb-symlink-jj-config-files-service-type
-                         home-xdg-configuration-files-service-type
-                         `(("jj/config.toml"
-                            ,(local-file "files/jujutsu/config.toml"))))
-         (service home-openssh-service-type
-                  (home-openssh-configuration
-                   (hosts
-                    (list
-                     (openssh-host (name "WindowsG14")
-                                   (host-name "192.168.4.138")
-                                   (user "krisbalintona")
-                                   (forward-x11? #t)
-                                   (forward-x11-trusted? #t))))))
+       (service home-fish-service-type)
+       (simple-service 'krisb-symlink-git-config-files-service-type
+           home-xdg-configuration-files-service-type
+         `(("git/config"
+            ,(local-file "files/git/config"))))
+       (simple-service 'krisb-symlink-jj-config-files-service-type
+           home-xdg-configuration-files-service-type
+         `(("jj/config.toml"
+            ,(local-file "files/jujutsu/config.toml"))))
+       (service home-openssh-service-type
+         (home-openssh-configuration
+           (hosts
+            (list
+             (openssh-host (name "WindowsG14")
+                           (host-name "192.168.4.138")
+                           (user "krisbalintona")
+                           (forward-x11? #t)
+                           (forward-x11-trusted? #t))))))
 
-          (service home-files-service-type
-           `((".guile" ,%default-dotguile)
-             (".Xdefaults" ,%default-xdefaults)))
+       (service home-files-service-type
+         `((".guile" ,%default-dotguile)
+           (".Xdefaults" ,%default-xdefaults)))
 
-          (service home-xdg-configuration-files-service-type
-           `(("gdb/gdbinit" ,%default-gdbinit)
-             ("nano/nanorc" ,%default-nanorc))))
+       (service home-xdg-configuration-files-service-type
+         `(("gdb/gdbinit" ,%default-gdbinit)
+           ("nano/nanorc" ,%default-nanorc))))
 
-        %base-home-services))))
+      %base-home-services))))
 
 home-config
--- conf/sublation.scm
+++ conf/sublation.scm
@@ -7,61 +7,61 @@
 ((compose (nonguix-transformation-guix)
           (nonguix-transformation-linux))
  (operating-system
-  (locale "en_US.utf8")
-  (timezone "America/Chicago")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "sublation")
+   (locale "en_US.utf8")
+   (timezone "America/Chicago")
+   (keyboard-layout (keyboard-layout "us"))
+   (host-name "sublation")
 
-  ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                 (name "krisbalintona")
-                 (comment "Kristoffer Balintona")
-                 (group "users")
-                 (home-directory "/home/krisbalintona")
-                 ;; REVIEW 2025-12-06: Fish shell through tramp has
-                 ;; failed despite all my attempts
-                 ;; (shell (file-append fish "/bin/fish"))
-                 (supplementary-groups '("wheel" "netdev" "audio" "video")))
-                %base-user-accounts))
+   ;; The list of user accounts ('root' is implicit).
+   (users (cons* (user-account
+                   (name "krisbalintona")
+                   (comment "Kristoffer Balintona")
+                   (group "users")
+                   (home-directory "/home/krisbalintona")
+                   ;; REVIEW 2025-12-06: Fish shell through tramp has
+                   ;; failed despite all my attempts
+                   ;; (shell (file-append fish "/bin/fish"))
+                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                 %base-user-accounts))
 
-  (packages
-   (cons glibc-locales
-         %base-packages))
+   (packages
+    (cons glibc-locales
+          %base-packages))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list
-            (service caddy-service-type
-                     (caddy-configuration
-	              (caddyfile (local-file "files/caddy/Caddyfile"))))
-            (service openssh-service-type)
-            (service network-manager-service-type)
-            (service wpa-supplicant-service-type)
-            (service ntp-service-type)
-            (service elogind-service-type
-                     (elogind-configuration
-                      (handle-lid-switch 'ignore)
-                      (handle-lid-switch-docked 'ignore)
-                      (handle-lid-switch-external-power 'ignore)
-                      (lid-switch-ignore-inhibited? #t))))
-           %base-services))
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
+   ;; Below is the list of system services.  To search for available
+   ;; services, run 'guix system search KEYWORD' in a terminal.
+   (services
+    (append (list
+             (service caddy-service-type
+               (caddy-configuration
+	         (caddyfile (local-file "files/caddy/Caddyfile"))))
+             (service openssh-service-type)
+             (service network-manager-service-type)
+             (service wpa-supplicant-service-type)
+             (service ntp-service-type)
+             (service elogind-service-type
+               (elogind-configuration
+                 (handle-lid-switch 'ignore)
+                 (handle-lid-switch-docked 'ignore)
+                 (handle-lid-switch-external-power 'ignore)
+                 (lid-switch-ignore-inhibited? #t))))
+            %base-services))
+   (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets (list "/boot/efi"))
+                 (keyboard-layout keyboard-layout)))
 
-  ;; The list of file systems that get "mounted".  The unique file
-  ;; system identifiers there ("UUIDs") can be obtained by running
-  ;; 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                        (mount-point "/")
-                        (device (uuid
-                                 "fc895b22-bd9a-4c70-8449-2aed8ed7a116"
-                                 'btrfs))
-                        (type "btrfs"))
-                       (file-system
-                        (mount-point "/boot/efi")
-                        (device (uuid "8D5D-5605"
-                                      'fat32))
-                        (type "vfat")) %base-file-systems))))
+   ;; The list of file systems that get "mounted".  The unique file
+   ;; system identifiers there ("UUIDs") can be obtained by running
+   ;; 'blkid' in a terminal.
+   (file-systems (cons* (file-system
+                          (mount-point "/")
+                          (device (uuid
+                                   "fc895b22-bd9a-4c70-8449-2aed8ed7a116"
+                                   'btrfs))
+                          (type "btrfs"))
+                        (file-system
+                          (mount-point "/boot/efi")
+                          (device (uuid "8D5D-5605"
+                                        'fat32))
+                          (type "vfat")) %base-file-systems))))
