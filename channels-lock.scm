4a512d1b46dfb34aca8d3c648572c587fc395458
Kristoffer Balintona <krisbalintona@gmail.com>
2025-12-09 17:38:53 -0600

    channels: Upgrade

--- channels-lock.scm
+++ channels-lock.scm
@@ -10,11 +10,22 @@
             (openpgp-fingerprint
               "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
       (channel
+        (name 'abbe)
+        (url "https://codeberg.org/group/guix-modules.git")
+        (branch "mainline")
+        (commit
+          "719e3aedc39b7c0b61795b8e18069be74536551c")
+        (introduction
+          (make-channel-introduction
+            "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
+            (openpgp-fingerprint
+              "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
+      (channel
         (name 'emacs-master)
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "2b4da3610cdcf58ed395a2c433b898489c87ae2b")
+          "4fffc7b81b6f9ab47534d88019230b74f147b41c")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -25,7 +36,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "0f68c1684169cbef8824fb246dfefa3e6832225b")
+          "d741f41eca0a04694775118eedaa332ffa1a26f8")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -36,7 +47,7 @@
         (url "https://codeberg.org/guix/guix.git")
         (branch "master")
         (commit
-          "12dabd127f6b2a9abc0514b5ceb04748f3a3788e")
+          "3a068fcbb316fa94dc1bf8f514a42f399ab04850")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
