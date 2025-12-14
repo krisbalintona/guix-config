8fdfea1fa0181a80d522a9c63a08868687988f97
Kristoffer Balintona <krisbalintona@gmail.com>
2025-12-14 04:14:38 -0600

    mute: Lock

---
+++ env/channels-lock-mute.scm
@@ -0,0 +1,55 @@
+(list (channel
+        (name 'sops-guix)
+        (url "https://github.com/fishinthecalculator/sops-guix")
+        (branch "main")
+        (commit
+          "381880aee336faf8d5e3562abced369c9ea1c3cc")
+        (introduction
+          (make-channel-introduction
+            "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+            (openpgp-fingerprint
+              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+      (channel
+        (name 'abbe)
+        (url "https://codeberg.org/group/guix-modules.git")
+        (branch "mainline")
+        (commit
+          "719e3aedc39b7c0b61795b8e18069be74536551c")
+        (introduction
+          (make-channel-introduction
+            "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
+            (openpgp-fingerprint
+              "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
+      (channel
+        (name 'emacs-master)
+        (url "https://github.com/gs-101/emacs-master.git")
+        (branch "main")
+        (commit
+          "4fffc7b81b6f9ab47534d88019230b74f147b41c")
+        (introduction
+          (make-channel-introduction
+            "568579841d0ca41a9d222a2cfcad9a7367f9073b"
+            (openpgp-fingerprint
+              "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "d741f41eca0a04694775118eedaa332ffa1a26f8")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://codeberg.org/guix/guix.git")
+        (branch "master")
+        (commit
+          "3a068fcbb316fa94dc1bf8f514a42f399ab04850")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
