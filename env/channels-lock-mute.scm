df69f21e25e7de81e682c443a2d56c1950fee4a6
Kristoffer Balintona <krisbalintona@gmail.com>
2026-03-01 21:00:55 -0600

    build: Upgrade mute lock file

--- env/channels-lock-mute.scm
+++ env/channels-lock-mute.scm
@@ -3,7 +3,7 @@
         (url "https://github.com/fishinthecalculator/sops-guix")
         (branch "main")
         (commit
-          "381880aee336faf8d5e3562abced369c9ea1c3cc")
+          "c208d19921bd93cccf12f581a9297fad0929c013")
         (introduction
           (make-channel-introduction
             "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
@@ -14,18 +14,29 @@
         (url "https://codeberg.org/group/guix-modules.git")
         (branch "mainline")
         (commit
-          "719e3aedc39b7c0b61795b8e18069be74536551c")
+          "be84fef32e25d1a1ee8cbfe32087786670fbc624")
         (introduction
           (make-channel-introduction
             "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
             (openpgp-fingerprint
               "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
       (channel
+        (name 'rosenthal)
+        (url "https://codeberg.org/hako/rosenthal.git")
+        (branch "trunk")
+        (commit
+          "f58f1b058338fafa52e164e97b62cf622aef00e4")
+        (introduction
+          (make-channel-introduction
+            "7677db76330121a901604dfbad19077893865f35"
+            (openpgp-fingerprint
+              "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+      (channel
         (name 'emacs-master)
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "4fffc7b81b6f9ab47534d88019230b74f147b41c")
+          "7ec18fb1a15d612c097405811bb9756e89034922")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -36,7 +47,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "d741f41eca0a04694775118eedaa332ffa1a26f8")
+          "816b5c6f45ed56cd81e2a6338ed5b710e4e66e98")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -47,7 +58,7 @@
         (url "https://codeberg.org/guix/guix.git")
         (branch "master")
         (commit
-          "3a068fcbb316fa94dc1bf8f514a42f399ab04850")
+          "c8985dd911c4c8e2948c888df8b54c9bc317625e")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
