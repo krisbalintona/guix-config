71cbfc3a7df2b9f2bd1e69962a7509ce26d3334a
Kristoffer Balintona <krisbalintona@gmail.com>
2026-03-26 21:46:44 -0500

    mute: build: Upgrade lock file

--- env/channels-lock-mute.scm
+++ env/channels-lock-mute.scm
@@ -1,66 +1,60 @@
 (list (channel
-        (name 'sops-guix)
-        (url "https://github.com/fishinthecalculator/sops-guix")
-        (branch "main")
-        (commit
-          "c208d19921bd93cccf12f581a9297fad0929c013")
-        (introduction
-          (make-channel-introduction
-            "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
-            (openpgp-fingerprint
-              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+       (name 'sops-guix)
+       (url "https://github.com/fishinthecalculator/sops-guix")
+       (branch "main")
+       (commit "e0d8d980df67bddc50cc98e6d5fb9fa72b779ec8")
+       (introduction
+        (make-channel-introduction
+         "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+         (openpgp-fingerprint
+          "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
       (channel
-        (name 'abbe)
-        (url "https://codeberg.org/group/guix-modules.git")
-        (branch "mainline")
-        (commit
-          "be84fef32e25d1a1ee8cbfe32087786670fbc624")
-        (introduction
-          (make-channel-introduction
-            "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
-            (openpgp-fingerprint
-              "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
+       (name 'abbe)
+       (url "https://codeberg.org/group/guix-modules.git")
+       (branch "mainline")
+       (commit "e4b66698e84293cd090b2cc2a10dc2062f4f7cd7")
+       (introduction
+        (make-channel-introduction
+         "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
+         (openpgp-fingerprint
+          "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
       (channel
-        (name 'rosenthal)
-        (url "https://codeberg.org/hako/rosenthal.git")
-        (branch "trunk")
-        (commit
-          "f58f1b058338fafa52e164e97b62cf622aef00e4")
-        (introduction
-          (make-channel-introduction
-            "7677db76330121a901604dfbad19077893865f35"
-            (openpgp-fingerprint
-              "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+       (name 'rosenthal)
+       (url "https://codeberg.org/hako/rosenthal.git")
+       (branch "trunk")
+       (commit "ca9a350d787695c54a277044c785b1fe84911408")
+       (introduction
+        (make-channel-introduction
+         "7677db76330121a901604dfbad19077893865f35"
+         (openpgp-fingerprint
+          "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
       (channel
-        (name 'emacs-master)
-        (url "https://github.com/gs-101/emacs-master.git")
-        (branch "main")
-        (commit
-          "7ec18fb1a15d612c097405811bb9756e89034922")
-        (introduction
-          (make-channel-introduction
-            "568579841d0ca41a9d222a2cfcad9a7367f9073b"
-            (openpgp-fingerprint
-              "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
+       (name 'emacs-master)
+       (url "https://github.com/gs-101/emacs-master.git")
+       (branch "main")
+       (commit "a015335df073227173a5c0e54ec25e7b4f9d3eca")
+       (introduction
+        (make-channel-introduction
+         "568579841d0ca41a9d222a2cfcad9a7367f9073b"
+         (openpgp-fingerprint
+          "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
       (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit
-          "816b5c6f45ed56cd81e2a6338ed5b710e4e66e98")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit "45bda95c22845cba67ffe63b4e61202fa122aaf5")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-        (name 'guix)
-        (url "https://codeberg.org/guix/guix.git")
-        (branch "master")
-        (commit
-          "c8985dd911c4c8e2948c888df8b54c9bc317625e")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+       (name 'guix)
+       (url "https://codeberg.org/guix/guix.git")
+       (branch "master")
+       (commit "be5617888e0c434c7595b8fd361c2015fbdba3f6")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
