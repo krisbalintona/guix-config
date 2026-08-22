6d80e2216c3ec5dd214d296ee187cede382777f2
Kristoffer Balintona <krisbalintona@gmail.com>
2026-08-22 18:42:13 -0500

    mute: chore: Upgrade

--- env/channels-lock-mute.scm
+++ env/channels-lock-mute.scm
@@ -12,7 +12,7 @@
        (name 'abbe)
        (url "https://codeberg.org/group/guix-modules.git")
        (branch "mainline")
-       (commit "ad3d33bd1ffc6ca0ceca0019005e1e003bbb0893")
+       (commit "6118b32a72848026ca270c81ba2e18fd8e77c48b")
        (introduction
         (make-channel-introduction
          "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
@@ -22,7 +22,7 @@
        (name 'rosenthal)
        (url "https://codeberg.org/hako/rosenthal.git")
        (branch "trunk")
-       (commit "c2722ed1762b9ff38ce77331019b660477f7dad3")
+       (commit "21391bab6e38561488bd807493da1035a85c24cb")
        (introduction
         (make-channel-introduction
          "7677db76330121a901604dfbad19077893865f35"
@@ -32,7 +32,7 @@
        (name 'emacs-master)
        (url "https://github.com/gs-101/emacs-master.git")
        (branch "main")
-       (commit "2cff0142870d5239d5fa048f2ed09b3b392b3ef1")
+       (commit "87391be798551db6b0bf0f60b19c018478012c51")
        (introduction
         (make-channel-introduction
          "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -42,7 +42,7 @@
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
        (branch "master")
-       (commit "4bc86c61d5ab661614b099bfe524f7f5798988b3")
+       (commit "caa8c0b4646b993537be13c9bc819b3df68ab9b2")
        (introduction
         (make-channel-introduction
          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -52,7 +52,7 @@
        (name 'guix)
        (url "https://codeberg.org/guix/guix.git")
        (branch "master")
-       (commit "ce9929fadfcdb1426eec4ea228aaf68db7fd2db3")
+       (commit "883d5b48010542f04eafdb9c8f03a8b17ea8ace4")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
